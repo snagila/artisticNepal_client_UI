67d5ed50bc54542420e6c181e08f2c337d4b683e
Sachin Nagila <sachinnagila2053@gmail.com>
2024-08-07 15:30:08 +1000

    demo header ready

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 import "./header.css";
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Stack } from "react-bootstrap";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaRegUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <>
-      <Row className="bg-primary">
-        <Col>hello</Col>
-        <Col>whatsup</Col>
+      <Row className="bg-primary header d-flex align-items-center justify-content-between fs-5">
+        <Col className="ms-5 ">
+          <Stack direction="horizontal" gap={2} className="fs-3">
+            <div>
+              <GiHamburgerMenu />
+            </div>
+            <div id="menu">Menu</div>
+          </Stack>
+        </Col>
+
+        <Col className="d-flex align-items-center justify-content-center flex-column">
+          <img src={logo} width={100} height={80} alt="" />
+        </Col>
+
+        <Col className="text-center d-flex  align-items-center justify-content-center ">
+          <Stack className="text-center  align-items-center justify-content-center">
+            <div>
+              <FaHeart />
+            </div>
+            <div>WishList</div>
+          </Stack>
+          <Stack>
+            <div>
+              <FaRegUser />
+            </div>
+            <div>Account</div>
+          </Stack>
+          <Stack>
+            <div>
+              <FaShoppingCart />
+            </div>
+            <div>Cart</div>
+          </Stack>
+        </Col>
       </Row>
     </>
   );
--- src/components/homepagebanner/HomepageBanner.jsx
+++ src/components/homepagebanner/HomepageBanner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./homePageBanner.css";
 
 const HomepageBanner = () => {
-  return <p id="homepagebanner">The arts of Nepal</p>;
+  return <div id="homepagebanner">The arts of Nepal</div>;
 };
 
 export default HomepageBanner;
